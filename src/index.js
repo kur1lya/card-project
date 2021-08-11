ef8b7fdbc94aae6819c4f028184489661865d997
Ilya <dodob6388@gmail.com>
2021-08-11 12:09:14 +0300

    update dependencies

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { slides, description_cards, products } from './massive'
+import {  description_cards, products } from './massive'
 import { harry_st} from './arcticles'
 import {Provider} from 'react-redux'
 import {store} from './Redux/index'
